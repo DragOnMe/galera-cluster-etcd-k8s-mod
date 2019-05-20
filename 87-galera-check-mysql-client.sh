43c20cd7c8255e6ee20d7c8d63eb80deca5dd170
Cloud User <centos@deploy-ctlr.novalocal>
2019-05-20 07:38:05 +0000

    k8s cluster domain change

--- 87-galera-check-mysql-client.sh
+++ 87-galera-check-mysql-client.sh
@@ -3,7 +3,7 @@
 kubectl run mysql-client --image=mysql:5.7 --restart=Never --env="MYSQL_ROOT_PASSWORD=temp"
 
 while true; do
-  kubectl exec -it mysql-client -- mysql -h galera-read.ns-galera-etcd.svc.mac-prod -uroot -pmysql -e 'select * from information_schema.GLOBAL_STATUS where VARIABLE_NAME like "wsrep_local_index" or VARIABLE_NAME like "wsrep_local_state_comment" or VARIABLE_NAME like "wsrep_connected" or VARIABLE_NAME like "wsrep_cluster_size" or VARIABLE_NAME like "wsrep_cluster_status" or VARIABLE_NAME like "wsrep_ready";' 
+  kubectl exec -it mysql-client -- mysql -h galera-read.ns-galera-etcd.svc.spc.local -uroot -pmysql -e 'select * from information_schema.GLOBAL_STATUS where VARIABLE_NAME like "wsrep_local_index" or VARIABLE_NAME like "wsrep_local_state_comment" or VARIABLE_NAME like "wsrep_connected" or VARIABLE_NAME like "wsrep_cluster_size" or VARIABLE_NAME like "wsrep_cluster_status" or VARIABLE_NAME like "wsrep_ready";' 
   echo '----------'
   sleep 10
 done
